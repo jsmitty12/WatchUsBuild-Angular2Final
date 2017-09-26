f41028e7c1558b133278689489ebafbb0808fac2
Joshua Smith <jsmith@intheround.com>
2017-09-26 08:45:08 -0400

    Move logic from Races component into Race component

--- src/app/race/race.component.ts
+++ src/app/race/race.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
+import {Race} from '../races/race';
+import {RacesComponent} from '../races/races.component';
 
 @Component({
-  selector: 'race',
-  templateUrl: './race.component.html',
-  styleUrls: ['./race.component.css']
+    selector: 'race',
+    templateUrl: './race.component.html',
+    styleUrls: ['./race.component.css']
 })
 export class RaceComponent implements OnInit {
 
-  constructor() { }
+    @Input() race: Race;
 
-  ngOnInit() {
-  }
+    constructor(private racesComponent: RacesComponent) {
+    }
 
+    ngOnInit() {
+    }
+
+    castDate(date) {
+        return new Date(date);
+    }
+
+    enterRace() {
+        this.racesComponent.enterRace(this.race);
+    }
+
+    cancelRace() {
+        this.racesComponent.cancelRace(this.race);
+    }
 }
